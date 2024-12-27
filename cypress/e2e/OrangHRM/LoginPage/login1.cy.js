e9e356048ac905ed1ebf8a1d45e396ced0e4a048
rifqi204-QA <rifqi204@gmail.com>
2024-12-28 03:36:27 +0700

    not use

--- cypress/e2e/OrangHRM/LoginPage/login1.cy.js
+++ cypress/e2e/OrangHRM/LoginPage/login1.cy.js
@@ -37,29 +37,29 @@ describe('login Feature', () => {
     //    });
     //    cy.get('[class="oxd-text oxd-text--p oxd-alert-content-text"]').should('have.text','Invalid credentials');
     // })
-    it('login dengan username terdaftar tetapi password Blank', () => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-        cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]').should('have.text', 'Login');
-        cy.get('[name="username"]').type('Admin'); 
-        cy.get('[name="password"]').should('have.value', '');    
-        cy.intercept("GET", "**/index.php/core/i18n/messages").as("messages");
-        cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
-        cy.get('.oxd-text--span', {timeout: 5000}).should('contain.text', 'Required');
-        cy.wait("@messages").then((intercept) => {
-            expect(intercept.response.statusCode).to.equal(200);
-        })
-    });
+    // it('login dengan username terdaftar tetapi password Blank', () => {
+    //     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    //     cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]').should('have.text', 'Login');
+    //     cy.get('[name="username"]').type('Admin'); 
+    //     cy.get('[name="password"]').should('have.value', '');    
+    //     cy.intercept("GET", "**/index.php/core/i18n/messages").as("messages");
+    //     cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    //     cy.get('.oxd-text--span', {timeout: 5000}).should('contain.text', 'Required');
+    //     cy.wait("@messages").then((intercept) => {
+    //         expect(intercept.response.statusCode).to.equal(200);
+    //     })
+    // });
     
-    it('login dengan username blank dan Password Blank', () => {
-       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-       cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]').should('have.text','Login');
-       cy.get('[name="username"]').should('have.value', '');
-       cy.get('[name="password"]').should('have.value', '');
-       cy.intercept("GET","**/index.php/core/i18n/messages").as("messages");
-       cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
-       cy.wait("@messages").then((intercept) => {
-           expect(intercept.response.statusCode).to.equal(200)
-       });
-       cy.get('[class="oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message"]').should('have.text','RequiredRequired');
-    })    
+    // it('login dengan username blank dan Password Blank', () => {
+    //    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    //    cy.get('[class="oxd-text oxd-text--h5 orangehrm-login-title"]').should('have.text','Login');
+    //    cy.get('[name="username"]').should('have.value', '');
+    //    cy.get('[name="password"]').should('have.value', '');
+    //    cy.intercept("GET","**/index.php/core/i18n/messages").as("messages");
+    //    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click();
+    //    cy.wait("@messages").then((intercept) => {
+    //        expect(intercept.response.statusCode).to.equal(200)
+    //    });
+    //    cy.get('[class="oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message"]').should('have.text','RequiredRequired');
+    // })
 })
